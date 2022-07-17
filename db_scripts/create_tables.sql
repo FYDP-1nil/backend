7e78a1737c499757f96bff647923762964560244
madhur4444 <mk3sharm@edu.uwaterloo.ca>
2022-07-17 11:55:52 -0400

    push sql schemas

--- db_scripts/create_tables.sql
+++ db_scripts/create_tables.sql
@@ -24,5 +24,68 @@ CREATE TABLE leagues (
 CREATE UNIQUE INDEX leagues_pkey ON leagues(id uuid_ops);
 CREATE UNIQUE INDEX leagues_league_name_key ON leagues(league_name text_ops);
 
+-- Create table soccer game
+CREATE TABLE soccergames (
+    id uuid PRIMARY KEY,
+    home character varying(255) NOT NULL,
+    away character varying(255) NOT NULL,
+    starttime datetime NOT NULL
+);
+
+-- Indices
+
+CREATE UNIQUE INDEX soccer_pkey ON soccergames(id uuid_ops);
+
+CREATE TABLE soccerevents (
+    id uuid PRIMARY KEY,
+    EventType character varying(255) NOT NULL,
+    GameId character varying(255) NOT NULL,
+    SocEvent blob(255) NOT NULL,
+);
+
+CREATE UNIQUE INDEX soccer_events_pkey ON soccerevents(id uuid_ops);
+
+
+CREATE TABLE soccershots (
+    id uuid PRIMARY KEY,
+    GameId uuid,
+    IsGoal boolean,
+	IsOnTarget boolean,
+	Player character varying(255) NOT NULL,
+	Assist character varying(255) NOT NULL,
+	Time integer NOT NULL, 
+	TeamFor character varying(255) NOT NULL,
+	TeamAgainst character varying(255) NOT NULL,
+);
+
+CREATE UNIQUE INDEX soccer_shots_pkey ON soccershots(id uuid_ops);
+
+CREATE TABLE soccerfouls (
+	Id uuid PRIMARY KEY,
+	GameId uuid,
+	TeamFor character varying(255) NOT NULL,
+	TeamAgainst character varying(255) NOT NULL,
+	Player character varying(255) NOT NULL, 
+	Reason character varying(255) NOT NULL,
+	IsYellow boolean,
+	IsRed boolean,
+	Time integer NOT NULL,
+); 
+
+CREATE UNIQUE INDEX soccer_fouls_pkey ON soccerfouls(id uuid_ops);
+
+CREATE TABLE socceroffsides (
+	Id uuid PRIMARY KEY,
+	GameId uuid,
+	TeamFor character varying(255) NOT NULL,
+	TeamAgainst character varying(255) NOT NULL,
+	Time integer NOT NULL,
+);
+
+CREATE UNIQUE INDEX soccer_off_sides_pkey ON socceroffsides(id uuid_ops);
+
+
+
+
 
 
