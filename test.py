90fc2482c8d68677a722b4211db16f8f98aa62e3
madhur4444 <mk3sharm@edu.uwaterloo.ca>
2023-02-17 18:56:08 -0500

    push long lived access token without any security in .py file, yoo bots ?

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 page_id_1 = 101787976183954
-facebook_access_token_1 = 'EAAILuM32DZBEBACPImGBo0rlpMXh5qrQxbnZAON4lWh6mrdmmRG6gj2e5f1aoOtJ3DbJZAAMXscvJ4YcxVu3PXeBYST4ZBgT5ZAVmxUidyDOnlybZBdXsx3ZAEZAox7R7cOxPhK3TLqPzTxUH85ZBuCfzoNMVAUeZASWrVE8bdagDtzcIzbOeD9ZCXZAx9ZCCf60lmrt2HSH1dxY44SnHrMzj90oazlp89X4qDarpC2eizKRebgZDZD'
+facebook_access_token_1 = 'EAAILuM32DZBEBANgvn1cD5swm8MgdBEUJ3nZBpxql6Ta6d2eD3wmuWsBKC30A7Fb1GTCIbfX0bZBh1ZCMeaVKbKZAQEzwi01d29yc7mmb7XWL7uMkyqaZCrMEgIYt8nqxi514ZCh7rQqzqPvEyicqAK5WWZBPGpwzYgUChWaHMMXNKl1ZBuohuheSC7dlP1E96WlpULr2UDP92xCeaU2itzIX'
 image_url = 'https://graph.facebook.com/{}/photos'.format(page_id_1)
 image_location = 'http://image.careers-portal.co.za/f_output.jpg'
 img_payload = {
