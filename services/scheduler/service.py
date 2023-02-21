4d1d74fa1b41463a4cc1d1c6dc25a57d36809c8f
madhur4444 <mk3sharm@edu.uwaterloo.ca>
2023-02-20 22:06:51 -0500

    add fb api working post integration in scheduler, update reqs, update testing

--- services/scheduler/service.py
+++ services/scheduler/service.py
@@ -2,16 +2,27 @@
 from concurrent import futures
 import logging
 import grpc
+import requests
 from ..gen import scheduler_pb2
 from ..gen import scheduler_pb2_grpc
 class Scheduler(scheduler_pb2_grpc.SchedulerServicer):
 
     def SetPost(self, request, context):
-        resp = schedule_post()
+        # send ret message recv from mantle
+        dummy_text_from_mantle = "Hi I am 1nil scheduler guys! Hope you like me"
+        resp = schedule_post(dummy_text_from_mantle)
         return scheduler_pb2.SetPostResponse(success=resp)
 
-def schedule_post():
-    pass
+def schedule_post(post_text):
+    page_id_1 = 101787976183954
+    facebook_access_token_1 = 'EAAILuM32DZBEBAEzXmfQKvjHyBcsYum2kiWOn7yAxcTxtSHKvJHfreIVGThZByL03p7Qw2IBG3ZCcZA6LIoXdgLaHRf41XMB5J0UCQfZCMwWDm8lJs9YBalq5HnsWZBLIHRfLtALZCmeBJDVU6DUSZAQnY4633H247bTrTidwyS1ORZCWBwzepnJoFYtrKFMcTET9NoZC7cvjwaKVp8BMRMk7PPeYIylZBogyc3rIyaazOjEAZDZD'
+    post_url = 'https://graph.facebook.com/{}/feed/'.format(page_id_1)
+    payload = {
+        'message': post_text,
+        'access_token': facebook_access_token_1
+    }
+    r = requests.post(post_url, payload)
+    return (r.status_code == 200)
 
 def serve(logger):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
--- test.py
+++ test.py
@@ -1,6 +1,29 @@
-# import requests
+import requests
 # page_id_1 = 101787976183954
-# facebook_access_token_1 = 'EAAILuM32DZBEBANgvn1cD5swm8MgdBEUJ3nZBpxql6Ta6d2eD3wmuWsBKC30A7Fb1GTCIbfX0bZBh1ZCMeaVKbKZAQEzwi01d29yc7mmb7XWL7uMkyqaZCrMEgIYt8nqxi514ZCh7rQqzqPvEyicqAK5WWZBPGpwzYgUChWaHMMXNKl1ZBuohuheSC7dlP1E96WlpULr2UDP92xCeaU2itzIX'
+# facebook_access_token_1 = 'EAAILuM32DZBEBAEzXmfQKvjHyBcsYum2kiWOn7yAxcTxtSHKvJHfreIVGThZByL03p7Qw2IBG3ZCcZA6LIoXdgLaHRf41XMB5J0UCQfZCMwWDm8lJs9YBalq5HnsWZBLIHRfLtALZCmeBJDVU6DUSZAQnY4633H247bTrTidwyS1ORZCWBwzepnJoFYtrKFMcTET9NoZC7cvjwaKVp8BMRMk7PPeYIylZBogyc3rIyaazOjEAZDZD'
+# # image_url = 'https://graph.facebook.com/{}/photos'.format(page_id_1)
+# post_url = 'https://graph.facebook.com/{}/feed/'.format(page_id_1)
+# # print(post_url)
+# # image_location = 'http://image.careers-portal.co.za/f_output.jpg'
+# payload = {
+# 'message': "Hi I am scheduler of 1 nil, i m cute",
+# 'access_token': facebook_access_token_1
+# }
+# #Send the POST request
+# r = requests.post(post_url, payload)
+# print((r.status_code == 200))
+
+page_id_1 = 101787976183954
+facebook_access_token_1 = 'EAAILuM32DZBEBAEzXmfQKvjHyBcsYum2kiWOn7yAxcTxtSHKvJHfreIVGThZByL03p7Qw2IBG3ZCcZA6LIoXdgLaHRf41XMB5J0UCQfZCMwWDm8lJs9YBalq5HnsWZBLIHRfLtALZCmeBJDVU6DUSZAQnY4633H247bTrTidwyS1ORZCWBwzepnJoFYtrKFMcTET9NoZC7cvjwaKVp8BMRMk7PPeYIylZBogyc3rIyaazOjEAZDZD'
+post_url = 'https://graph.facebook.com/{}/feed/'.format(page_id_1)
+payload = {
+    'message': "For 40 yards....KICK IS GOOD!!!\n\nEagles [3] - Giants 0",
+    'access_token': facebook_access_token_1
+}
+r = requests.post(post_url, payload)
+print((r.status_code == 200))
+# page_id_1 = 101787976183954
+# facebook_access_token_1 = 'EAAILuM32DZBEBAEzXmfQKvjHyBcsYum2kiWOn7yAxcTxtSHKvJHfreIVGThZByL03p7Qw2IBG3ZCcZA6LIoXdgLaHRf41XMB5J0UCQfZCMwWDm8lJs9YBalq5HnsWZBLIHRfLtALZCmeBJDVU6DUSZAQnY4633H247bTrTidwyS1ORZCWBwzepnJoFYtrKFMcTET9NoZC7cvjwaKVp8BMRMk7PPeYIylZBogyc3rIyaazOjEAZDZD'
 # image_url = 'https://graph.facebook.com/{}/photos'.format(page_id_1)
 # image_location = 'http://image.careers-portal.co.za/f_output.jpg'
 # img_payload = {
